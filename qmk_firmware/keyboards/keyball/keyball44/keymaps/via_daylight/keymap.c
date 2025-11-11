58becc11045d47303970adbcafbc970ee8a9d095
Daylight <33833751+daylight-pro@users.noreply.github.com>
2025-11-11 16:50:48 +0900

    修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_daylight/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_daylight/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
-#include SYM_SPC LT(1, KC_SPC)
+#define SYM_SPC LT(1, KC_SPC)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(4)    ,
-              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)    ,LT(1,KC_SPC),MO(3),                                        KC_BSPC,LT(2,KC_ENT), KC_J,     KC_RALT  , KC_PSCR
+              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)    ,SYM_SPC,MO(3),                                        KC_BSPC,LT(2,KC_ENT), KC_J,     KC_RALT  , KC_PSCR
   ),
 
   [1] = LAYOUT_universal(
